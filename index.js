8839a96f77402de145cf87e553c4d32e4125fbb4
Oliver <ollietheexplorer@gmail.cm>
2026-07-26 18:45:10 +1000

    added loading

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmp9rg69hmx.js
+// include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmphy9afsmx.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -162,21 +162,21 @@ Module['FS_createPath']("/", "fonts", true, true);
 
   })();
 
-// end include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmp9rg69hmx.js
-// include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmp0rczl0zf.js
+// end include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmphy9afsmx.js
+// include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmpa6ghul8n.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmp0rczl0zf.js
-// include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmpzrkk7rep.js
+  // end include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmpa6ghul8n.js
+// include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmp463w1mlq.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmpzrkk7rep.js
+  // end include: /var/folders/0w/brfrkkqj3lsc4rms3__bg5_r0000gn/T/tmp463w1mlq.js
 
 
 var arguments_ = [];
